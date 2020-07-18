de217bb42e618a32e781ce9313289921a4d1cc3e
Michael Hernandez <michael.hernandez27@outlook.com>
2020-07-18 11:37:18 -0400

    got react app running and edited contact page

--- client/greystone/src/App.js
+++ client/greystone/src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
-import home from "../components/home/home.js"
-import navbar from "../components/navbar/navbar.js"
-import contact from "../components/contact/contact.js"
-import reviews from "../components/reviews/reviews.js"
+import home from "./components/home/home.js"
+import navbar from "./components/navbar/navbar.js"
+import contact from "./components/contact/contact.js"
+import reviews from "./components/reviews/reviews.js"
+import Navbar from "./components/navbar/navbar.js";
 
 function App() {
     return (
         <Router>
           <div>
+          <Navbar/>
             <Switch>
                 <Route exact path="/" component={home} />
                 <Route exact path="/navbar" component={navbar} />
--- client/greystone/src/components/contact/contact.js
+++ client/greystone/src/components/contact/contact.js
@@ -1,14 +1,45 @@
-import React from 'react';
-import '../components/contact/contact.css'
+import React, { Fragment } from 'react';
+import './contact.css'
 
 const Contact =()=> {
+    
+    //insert functionality of contact page
+
     return (
-      <div>
+        <div class="container"/> 
+            <div style="text-align:center">
+                <h2>Contact Us</h2>
+                <p>Swing by for a tour of the workshop, or leave us a message:</p>
+        </div>
+    <div class="row"/>
+        <div class="column"/>
+            <img src="/greystone picture?" style="width:100%"/>
+        <div/>
+    <div class="column"/>
+      <form action="/action_page.php"/>
+        
+        <label for="name">Name</label>
+        <input type="text" id="name" name="name" placeholder="Your name.."/>
+        
+        <label for="email">Email</label>
+        <input type="text" id="email" name="email" placeholder="Your email.."/>
+        
+        <label for="phone">Phone Number</label>
+        <input type="text" id="phone" name="phone" placeholder="Your phone number.."/>
+
+        <label for="location">City of Location</label>
+        <input type="text" id="location" name="location" placeholder="Your city of location.."/>
+        
+        <label for="comment">Comments/Questions</label>
+        <textarea id="comment" name="comment" placeholder="Write something.." style="height:170px"></textarea>
+        
+        <input type="submit" value="Submit"/>
+      <form/>
+    <div/>
+  <div/>
+<div/>
+     
+        );
+    }
   
-      <div><p>Contact Page</p></div>
-      
-      </div>
-    );
-  }
-  
-  export default Contact;
+  export default Contact
