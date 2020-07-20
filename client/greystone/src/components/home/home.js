64a7000449885450a69c63dc8ca496747e4e4615
Michael Hernandez <michael.hernandez27@outlook.com>
2020-07-20 16:42:55 -0400

    edited home html

--- client/greystone/src/components/home/home.js
+++ client/greystone/src/components/home/home.js
@@ -3,12 +3,37 @@ import './home.css';
 
 const Home =()=> {
   return (
-    <div>
-    <h1>Greystone Custom Cabinetry</h1> 
+<>    
+<body/>
 
-    <div><p>Hello World!</p></div>
-    </div>
+<div class="header">
+  <h1>Greystone Custom Cabinetry</h1>
+</div>
+
+<div class="navbar">
+  <a href="../components/home/home.js" class="active">Home</a>
+  <a href="./components/contact.contact.js">Contact</a>
+  <a href="./components/reviews/reviews.js">Reviews</a>
+</div>
+
+<div class="row"/>
+  <div class="side"/>
+    <h2>About Us</h2>
+    <h5>Photo of front of shop sign</h5>
+    <div class="fakeimg" style="height:200px;">Image</div>
+    <p>add information about our shop</p>
+  <div/>
+  <div class="main"/>
+    <h2>Let us come and make your dream come true for your home with our exceptional work!</h2>
+    <div class="fakeimg" style="height:200px;">Image</div>
+    <p>insert images of past jobs</p>
+    <br/>
+  <div/>
+<div/>
+
+<body/>
+</>
   );
 }
 
-export default Home;
+export default Home
